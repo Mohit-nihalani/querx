f946b483e9a58db70160bab3c6b7af4e24ef8c68
Mohit Nihalani <m9922772770@gmail.com>
2022-11-01 19:26:38 +0530

    treatmentPage

--- src/TreatmentPage.js
+++ src/TreatmentPage.js
@@ -5,24 +5,12 @@ import CSatisfaction from './components/treatmentComponents/CSatisfaction'
 import TCounter from './components/treatmentComponents/TCounter';
 import Footer from './components/Footer';
 import LearnMore from './components/treatmentComponents/LearnMore';
+import TreatmentHero from './components/treatmentComponents/TreatmentHero';
 
 
 const TreatmentPage = () => {
   return (<>
-    <div className="container-fluid treatmentPageSection">
-    <div className="container shadow rounded">
-    <div className="row ">
-    <div className="col d-flex flex-column align-items-start justify-content-center"> 
-    <h3>What is?</h3>
-    <h1>Erectile Dysfunction?</h1>
-    <p>ED is defined as the inability to sustain an erection sufficient for sexual intercourse. The condition can affect anyone at any age.</p>
-    <button className="btn btn-lg bg-primary text-white rounded-pill">Contact Us</button>
-    </div>
-    <div className="col p-0"> 
-    <img className="img-fluid" src="https://quer.vercel.app/static/media/trtmnt01.641bca482fbcf12e518c.png"/></div>
-    </div>
-    </div>
-    </div>
+    <TreatmentHero/>
     <TCounter/>
     <LearnMore/>
     <CSatisfaction/>
--- src/components/treatmentComponents/TCounter.js
+++ src/components/treatmentComponents/TCounter.js
@@ -2,19 +2,19 @@ import React from 'react'
 
 const TCounter = () => {
   return (
-    <div className='container my-5'>
-        <div className='row gx-2'>
-            <div className='col'>
+    <div className="container my-5">
+        <div className='row'>
+            <div className='col-12 col-md-4 my-1 border border-success d-flex flex-column justify-content-center align-items-center'>
                 <h1 className='counterTitle'>49%</h1>
-                <p className='text-wrap' style={{width:'18rem'}}>men over the age of 40 experience erectile dysfunction.</p>
+                <p className='text-break text-center'>men over the age of 40 experience <br/>erectile dysfunction.</p>
             </div>
-            <div className='col'>
+            <div className='col-12 col-md-4 my-1 border border-success d-flex flex-column justify-content-center align-items-center'>
                 <h1 className='counterTitle'>91%</h1>
-                <p className='text-wrap' style={{width:'18rem'}}>people are on wrong or ineffective treatment for them.</p>
+                <p className='text-break text-center'>people are on wrong or ineffective<br/> treatment for them.</p>
             </div>
-            <div className='col'>
+            <div className='col-12 col-md-4 my-1 border border-success d-flex flex-column justify-content-center align-items-center'>
                 <h1 className='counterTitle'>57%</h1>
-                <p className='text-wrap' style={{width:'18rem'}}>men with diabetes experience erectile dysfunction.</p>
+                <p className='text-break text-center'>men with diabetes experience <br/> erectile dysfunction..</p>
             </div>
         </div>
     </div>
